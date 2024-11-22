843bb0c4949f5cf72756d9910b4f3a6cb2ccc395
Leah <leahvanderwesten@gmail.com>
2024-11-22 12:34:34 +0100

    refactor: 💡 writeline function toegevoegd

---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+
+        internal void Run()
+        {
+            Console.WriteLine("Dit is nu de start van mijn programma");
+        }
+    }
+}
