3c5b0c6beb346f3c8f0fd46142789ef165bfccb7
Leah <leahvanderwesten@gmail.com>
2024-11-22 13:02:41 +0100

    refactor: 💡 functions toegevoegd

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -7,10 +7,63 @@
             Program program = new Program();
             program.Run();
         }
+        internal void Vraag1()
+        {
+            Console.WriteLine("What's nine plus ten?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag2()
+        {
+            Console.WriteLine("Would you consider pizza to be good?");
+            string antwoord = Console.ReadLine();
+           
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag3()
+        {
+            Console.WriteLine("What do you think of politics?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag4()
+        {
+            Console.WriteLine("Do you like rhythm games?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag5()
+        {
+            Console.WriteLine("Do you like Osu! ?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag6()
+        {
+            Console.WriteLine("What is your favorite movie?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
 
         internal void Run()
         {
-            Console.WriteLine("Dit is nu de start van mijn programma");
+            Vraag1();
+            Vraag2();
+            Vraag3();
+            Vraag4();
+            Vraag5();
+            Vraag6();
         }
+
     }
 }
