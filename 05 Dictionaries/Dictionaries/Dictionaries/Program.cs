991fd087218393e0a1ccc497acc2e03f7465a94f
Benji <106337182+Fisjes@users.noreply.github.com>
2025-01-31 12:33:49 +0100

    refactor: 💡 Dictionaries gemaakt
    jajajaj

---
+++ 05 Dictionaries/Dictionaries/Dictionaries/Program.cs
@@ -0,0 +1,12 @@
+﻿Dictionary<int,string> personeelOpNummer = new Dictionary<int,string>();
+
+personeelOpNummer.Add(382942, "jim");
+personeelOpNummer.Add(789432, "tim");
+
+string jim = personeelOpNummer[382942];
+string tim = personeelOpNummer[789432];
+
+foreach (KeyValuePair<int, string> item in personeelOpNummer)
+{
+
+}
