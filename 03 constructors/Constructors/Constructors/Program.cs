f986d1af72b3896f23b756b99ec9da50f9632d24
Benji <106337182+Fisjes@users.noreply.github.com>
2025-01-31 12:25:32 +0100

    refactor: 💡 constructors gemaakt
    jajajajaja

---
+++ 03 constructors/Constructors/Constructors/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+private void Run()
+{
+    QuizVraag quizVraag = new QuizVraag("hier komt te vraag", "hier komt het antwoord");
+}
---
+++ 03 constructors/Constructors/Constructors/QuizVraag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class QuizVraag
+    {
+        internal string vraag;
+        internal string antwoord;
+    }
+}
+
+class QuizVraag
+{
+    internal string vraag;
+    internal string antwoord;
+
+    internal QuizVraag(string vraag, string antwoord)
+    {
+        this.vraag = vraag;
+        this.antwoord = antwoord;
+    }
+}
